f28e3d5c24bf19c9a0520c390d4cd8ae94289484
Phong Cao <phongcao0714@gmail.com>
2023-01-12 19:02:44 -0800

    Display musicXML files

--- src/components/score-display/displayPage.js
+++ src/components/score-display/displayPage.js
@@ -1,6 +1,55 @@
 import './displayPage.css'
 import playButton from './static/Play-Button.svg'
 import { useEffect } from 'react';
+import testFile from './Dichterliebe01.musicxml'
+
+import Embed from 'flat-embed'
+import OpenSheetMusicDisplay from '../../lib/OpenSheetMusicDisplay'
+
+//Flat.io
+// const renderScore = () => {
+//     const container = document.getElementById('score')
+//     const appId = '63bf79cf24013e27391d7a98'
+    
+//     const embed = new Embed(container, {
+//     embedParams: {
+//         appId,
+//         layout: 'track',
+//         videoPosition: 'float',
+//         controlsPosition: 'top',
+//     }
+//     });
+
+//     fetch(testFile)
+//     .then(response => {
+//         console.log(response)
+//         return response.text()
+//     }).then(xml => {
+//         console.log()
+//         embed.loadMusicXML(xml)
+//     })
+//     .then(() => console.log('Embed loaded'))
+//     .catch(error => console.error(error));
+
+// }
+
+
+//OpenSheetMusicDisplay
+// const renderScore = () => {
+//     var osmd = new OpenSheetMusicDisplay("score");
+//     osmd.setOptions({
+//         backend: "svg",
+//         drawTitle: true,
+//         // drawingParameters: "compacttight" // don't display title, composer etc., smaller margins
+//       });
+//       osmd
+//         .load(testFile)
+//         .then(
+//           function() {
+//             osmd.render();
+//           }
+//         );
+// }
 
 const DisplayPage = () => {
     useEffect(() => {
@@ -9,6 +58,8 @@ const DisplayPage = () => {
         document.getElementById("root").className = "DisplayPageBody";
         const app = document.getElementsByClassName("App")
         app[0].setAttribute('id', 'DisplayPageBody')
+        // renderScore()
+
     }, []);
 
     return (
@@ -18,11 +69,9 @@ const DisplayPage = () => {
             </div>
 
             <div className="display-window">
-                <div className="score-title">
-                    Score Name
-                </div>
-                <div className="score">
-
+                
+                <div className="score" id='score'>
+                    <OpenSheetMusicDisplay file={testFile}></OpenSheetMusicDisplay>
                 </div>
                 <div className="play-bar">
                     <button></button>
@@ -31,5 +80,7 @@ const DisplayPage = () => {
         </main>
     )
 }
+
+  
 
 export {DisplayPage};
---
+++ src/lib/OpenSheetMusicDisplay.jsx
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import { OpenSheetMusicDisplay as OSMD } from 'opensheetmusicdisplay';
+
+class OpenSheetMusicDisplay extends Component {
+    constructor(props) {
+      super(props);
+      this.state = { dataReady: false };
+      this.osmd = undefined;
+      this.divRef = React.createRef();
+    }
+  
+    setupOsmd() {
+      const options = {
+        // renderSingleHorizontalStaffline: true,
+        autoResize: true,
+
+        drawMeasureNumbers: 4,
+        measureToDrawRangeNeedsReset: 4,
+
+        showControls: true,
+        showExportPdfControl: false,
+        showPageFormatControl: false,
+        showZoomControl: true,
+        showHeader: true,
+        showDebugControls: false,
+      }
+      this.osmd = new OSMD(this.divRef.current, options);
+      this.osmd.load(this.props.file).then(() => this.osmd.render());
+    }
+  
+    resize() {
+      this.forceUpdate();
+    }
+  
+    componentWillUnmount() {
+      window.removeEventListener('resize', this.resize)
+    }
+  
+    componentDidUpdate(prevProps) {
+      if (this.props.drawTitle !== prevProps.drawTitle) {
+        this.setupOsmd();
+      } else {
+        this.osmd.load(this.props.file).then(() => this.osmd.render());
+      }
+      window.addEventListener('resize', this.resize)
+    }
+  
+    // Called after render
+    componentDidMount() {
+      this.setupOsmd();
+    }
+  
+    render() {
+      return (<div ref={this.divRef} />);
+    }
+  }
+
+  export default OpenSheetMusicDisplay;
