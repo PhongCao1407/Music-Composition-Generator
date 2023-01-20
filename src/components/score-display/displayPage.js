e972ad90d9e7ff72af4c295c6fdc4cc6616af3f9
Phong Cao <phongcao0714@gmail.com>
2023-01-20 12:29:58 -0800

    Add files and extract component functions

--- src/components/score-display/displayPage.js
+++ src/components/score-display/displayPage.js
@@ -3,84 +3,30 @@ import playButton from './static/Play-Button.svg'
 import { useEffect } from 'react';
 import services from '../../services/testService';
 
-import testFile from './static/chopin2810.mxl'
-// import testFile from './static/Dichterliebe01.musicxml'
+import testFile from './static/Classical/Classical_String_Quartet_No.15_in_D_minor_K.421:417b_W.A.Mozart_.mxl'
+import { appID } from './appID';
+
 
 import Embed from 'flat-embed'
 
+//Components
 
-//Flat.io
-const renderScore = () => {
-    const container = document.getElementById('score')
-    const appId = '63bf79cf24013e27391d7a98'
-    
-    const embed = new Embed(container, {
-    embedParams: {
-        appId,
-        layout: 'responsive',
-        videoPosition: 'float',
-        controlsPosition: 'bottom',
-        themePrimary: '#353535',
-    }
-    });
-
-
-    
-
-    // // Test Local File on Frontend
-    // const fileExtension = testFile.split('.').pop();
-    // console.log(testFile)
-    // if (fileExtension === "mxl") {
-    //     fetch(score)
-    //     .then(function (response) {
-    //         console.log(response)
-    //         return response.arrayBuffer();
-    //     })
-    //     .then(function (mxl) {
-    //     // Got the compressed score as an `ArrayBuffer`, load it in the embed
-    //         return embed.loadMusicXML(mxl);
-    //     })
-    //     .then(function () {
-    //     // Score loaded in the embed
-    //     })
-    //     .catch(function (error) {
-    //     // Unable to load the score
-    // });
-    // }
-    // else {
-    //     fetch(score)
-    //     .then(response => {
-    //         console.log(response)
-    //         return response.text()
-    //     }).then(xml => {
-    //         console.log()
-    //         embed.loadMusicXML(xml)
-    //     })
-    //     .then(() => console.log('Embed loaded'))
-    //     .catch(error => console.error(error));
-    // }    
-
-
-
-    // Get file from backend
-    // Because getting the file extension is rather difficult, default to mxl file for now
-
-    
-    fetch('http://localhost:3001/romantic')
-    .then(response => {
-        console.log(response)
-        return response.arrayBuffer()
-    })
-    .then(xml => {
-        embed.loadMusicXML(xml)
-    })
-    .then(() => console.log('Embed loaded'))
-    .catch(error => console.error(error))
-
-        
+const Genre = (props) => {
+    return (
+        <div className="music-type">
+            <h1 id="genre"></h1>
+        </div>
+    )
 }
 
-
+const PieceTitle = (props) => {
+    return (
+        <div className="piece-title">
+            <h2 id="title"></h2>
+            <h3 id="composer"></h3>
+        </div>
+    )
+}
 
 
 const DisplayPage = () => {
@@ -96,14 +42,10 @@ const DisplayPage = () => {
 
     return (
         <main id="displayPage">
-            <div className="music-type">
-                <h1>Romantic</h1>
-            </div>
+            <Genre/>
 
             <div className="display-window">
-                <div className="piece-title">
-                    <h2>Prelude, Op. 28, No. 10</h2>
-                </div>
+                <PieceTitle/>
                 <div className="score" id='score'>
                 </div>
                 
@@ -111,6 +53,126 @@ const DisplayPage = () => {
         </main>
     )
 }
+
+
+//Helper Functions
+
+//Flat.io
+const renderScore = () => {
+    //Configure container
+    const embed = configureEmbeddedContainer()
+
+    //Get and set piece details
+    const pieceDetails = getPieceDetails(testFile)
+    setPieceDetails(pieceDetails.genre, pieceDetails.pieceTitle, pieceDetails.composerName)
+
+    // Embed local File on Frontend
+    loadLocalFile(testFile, embed)    
+   
+}
+
+const configureEmbeddedContainer = () => {
+    const container = document.getElementById('score')
+    const appId = appID
+    
+    //Configuring embedded container
+    const embed = new Embed(container, {
+    embedParams: {
+        appId,
+        layout: 'responsive',
+        videoPosition: 'float',
+        controlsPosition: 'bottom',
+        themePrimary: '#353535',
+        hideTempo: true
+    }
+    });
+    
+    return embed
+}
+
+//Get genre, title, composer name from file
+const getPieceDetails = (file) => {
+    const fileSplit = file.split('_')
+
+    fileSplit[0] = fileSplit[0].split('/').pop()
+    
+    const genre = fileSplit[0]
+    // console.log(fileSplit)
+    const pieceTitle = fileSplit.splice(1, fileSplit.length - 3).join(" ")
+    // console.log(pieceTitle)
+    const composerName = fileSplit[fileSplit.length - 2]
+
+    return {genre, pieceTitle, composerName}
+}
+
+
+const setPieceDetails = (genre, pieceTitle, composerName) => {
+    const genreComponent = document.getElementById('genre')
+    const scoreTitleComponent = document.getElementById('title')
+    const scoreComposerNameComponent = document.getElementById('composer')
+    genreComponent.textContent = genre
+    scoreTitleComponent.textContent = pieceTitle
+    scoreComposerNameComponent.textContent = composerName
+}
+
+const loadLocalFile = (file, embed) => {
+    
+    const fileExtension = file.split('.').pop();
+    
+    console.log(file)
+    if (fileExtension === "mxl") {
+        fetch(file)
+        .then(function (response) {
+            console.log(response)
+            return response.arrayBuffer();
+        })
+        .then(function (mxl) {
+        // Got the compressed score as an `ArrayBuffer`, load it in the embed
+            embed.loadMusicXML(mxl);
+            
+        })
+        .then(function () {
+        // Score loaded in the embed
+            embed.getParts().then(function (parts) {
+                console.log('parts')
+            });
+        
+        })
+        .catch(function (error) {
+        // Unable to load the score
+    });
+    }
+    else {
+        fetch(file)
+        .then(response => {
+            console.log(response)
+            return response.text()
+        }).then(xml => {
+            console.log()
+            embed.loadMusicXML(xml)
+        })
+        .then(() => console.log('Embed loaded'))
+        .catch(error => console.error(error));
+    }  
+}
+
+// Get file from backend
+// Because getting the file extension is rather difficult, default to mxl file for now
+const loadNonlocalFile = (file, embed, url) => {
+    //Example url: http://localhost:3001/romantic
+    fetch(url)
+    .then(response => {
+        console.log(response)
+        return response.arrayBuffer()
+    })
+    .then(xml => {
+        embed.loadMusicXML(xml)
+    })
+    .then(() => console.log('Embed loaded'))
+    .catch(error => console.error(error))
+}
+
+
 
   
 
