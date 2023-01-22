efdd113e1304c3f20d3454e54831cbf7c878a9e7
Phong Cao <phongcao0714@gmail.com>
2023-01-21 16:44:44 -0800

    Update backend connection to get file headers

--- src/components/score-display/displayPage.js
+++ src/components/score-display/displayPage.js
@@ -3,9 +3,8 @@ import playButton from './static/Play-Button.svg'
 import { useEffect } from 'react';
 import services from '../../services/testService';
 
-import testFile from './static/Classical/Classical_String_Quartet_No.15_in_D_minor_K.421:417b_W.A.Mozart_.mxl'
+import testFile from './static/Baroque/Baroque_Winter_Largo_easy_arr._for_Small_Ensemble-_Flute_Violin_Cello_A.Vivaldi_.mxl'
 import { appID } from './appID';
-
 
 import Embed from 'flat-embed'
 
@@ -62,12 +61,16 @@ const renderScore = () => {
     //Configure container
     const embed = configureEmbeddedContainer()
 
-    //Get and set piece details
-    const pieceDetails = getPieceDetails(testFile)
-    setPieceDetails(pieceDetails.genre, pieceDetails.pieceTitle, pieceDetails.composerName)
 
-    // Embed local File on Frontend
-    loadLocalFile(testFile, embed)    
+    // // Embed local File on Frontend
+    // // Get and set piece details
+    // const pieceDetails = getPieceDetails(testFile)
+    // setPieceDetails(pieceDetails.genre, pieceDetails.pieceTitle, pieceDetails.composerName)
+    // loadLocalFile(testFile, embed)    
+
+    //Get file from dev backend
+    const url = 'http://localhost:3001/baroque/'
+    loadNonlocalFile(embed, url)
    
 }
 
@@ -158,11 +161,19 @@ const loadLocalFile = (file, embed) => {
 
 // Get file from backend
 // Because getting the file extension is rather difficult, default to mxl file for now
-const loadNonlocalFile = (file, embed, url) => {
+const loadNonlocalFile = (embed, url) => {
     //Example url: http://localhost:3001/romantic
     fetch(url)
     .then(response => {
-        console.log(response)
+        console.log(response.headers.get('genre'))
+        console.log(response.headers.get('piecetitle'))
+        console.log(response.headers.get('composername'))
+
+        const genre = response.headers.get('genre')
+        const pieceTitle = response.headers.get('piecetitle')
+        const composerName = response.headers.get('composername')
+        setPieceDetails(genre, pieceTitle, composerName)
+
         return response.arrayBuffer()
     })
     .then(xml => {
